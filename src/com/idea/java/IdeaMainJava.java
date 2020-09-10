957544e9120edd9fef53758b4de4aad06b1297f7
zhangliao613 <zhangliao613@163.com>
2020-09-10 09:22:49 +0800

    IdeaGitDemo mainjava

--- src/com/idea/java/IdeaMainJava.java
+++ src/com/idea/java/IdeaMainJava.java
@@ -2,6 +2,6 @@ package com.idea.java;
 
 public class IdeaMainJava {
     public static void main(String[] args) {
-        System.out.println("idea git demo");
+        System.out.println("idea git demo2");
     }
 }
